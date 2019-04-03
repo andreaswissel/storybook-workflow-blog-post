f96e8aef0888ef8672190a3d793749ac43bc3e97
Andreas Wissel <kaeku314@gmail.com>
2019-04-03 19:33:35 +0200

    add knobs

--- src/app/input/input.component.stories.ts
+++ src/app/input/input.component.stories.ts
@@ -1,7 +1,13 @@
-import { storiesOf } from '@storybook/angular';
-import { InputComponent } from './input.component';
+import {storiesOf} from '@storybook/angular';
+import {withKnobs, text} from '@storybook/addon-knobs';
+import {InputComponent} from './input.component';
 
 storiesOf('Input', module)
-    .add('Default', () => ({
-        component: InputComponent
-    }));
+  .addDecorator(withKnobs)
+  .add('Default', () => ({
+    component: InputComponent,
+    props: {
+      label: text('Label', 'The label', 'General'),
+      placeholder: text('Placeholder', 'The placeholder', 'General')
+    }
+  }));
